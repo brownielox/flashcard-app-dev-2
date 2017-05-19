99c0ffea558fad927c9b6f26088eec699d151258
brownielox <mrsmpython@gmail.com>
2017-05-18 22:14:36 -0400

    separation of New Subject and New Card

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,24 +1,21 @@
 class SubjectsController < ApplicationController
-  before_filter :authenticate_user!
-  load_and_authorize_resource only: [:edit, :show, :update]
+  before_action :authenticate_user!
 
   def index
-    @subjects = Subject.all
+    @subjects = Subject.where(:user_id => current_user.id)
   end
 
   def show
     @subject = Subject.find(params[:id])
     @card = Card.where(:subject_id => @subject.id).first
+    redirect_to new_card_path
   end
 
   def create
-    @subject = Subject.new(params[:name])
+    @subject = Subject.new(subject_params)
     @subject.user_id = current_user.id
-    if @subject.user_id == nil
-      puts "Sorry Charlie"
-    else
-      @subject.save
-    end
+    @subject.save
+    redirect_to @subject
   end
 
   def new
@@ -26,7 +23,7 @@ class SubjectsController < ApplicationController
 
   private
     def subject_params
-      params.require(:card).permit(:name, :user_id)
+      params.require(:subject).permit(:name, :user_id)
     end
 
 end
