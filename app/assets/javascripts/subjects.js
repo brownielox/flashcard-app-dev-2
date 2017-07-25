fe32bb040702026bf81533515e28cdd53cf58880
brownielox <mrsmpython@gmail.com>
2017-07-24 20:03:24 -0400

    added function to show subjects via serializer

--- app/assets/javascripts/subjects.js
+++ app/assets/javascripts/subjects.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
   //use get method to ask for all subjects
+  // $().click(function(subjects_button_click)){
+  $("#subjects_button").click(function() {
+    showSubjects();
+  });
+})
 
-  $("#subjects_button").click(function(){
+  function showSubjects(){
     $.get(`/subjects.json`, function(subjects_json_from_api_request){
       console.log(subjects_json_from_api_request);
+      $("#subjects_button").hide();
       $("#main").html("");
       subjects_json_from_api_request.forEach(function(subject){
-        $("#main").append(`<a href=subjects/${subject.id}/cards><p>${subject.name}</p></a>`);
+      $("#main").append(`<a href=subjects/${subject.id}/cards><p>${subject.name}</p></a>`);
       });
     });
-  });
-});
+  };
+
+  // function showCards(){
+  //   $.get(`/subjects/${subject.id}/cards.json`, function(cards_json_from_api_request){
+  //     $("#main").html("");
+  //     cards_json_from_api_request.forEach(function(card){
+  //       $("#main").append(`<a href=subjects/${subject.id}/cards><p>${card.front}</p></a>`);
+  //     });
+  //   });
+  // });
